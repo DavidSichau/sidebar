5419344da6b6069c2c684c5d600c2de3f2ebe10f
David Sichau <git@sichau.eu>
2015-04-28 17:23:47 +0200

    fixed order of inclusion

--- package.js
+++ package.js
@@ -1,16 +1,23 @@
 Package.describe({
   name: "davidsichau:sidebar",
   summary: "Provide a simple sidebar.",
-  version: "0.1.0",
+  version: "0.1.1",
   git: "https://github.com/DavidSichau/meteor-sidebar"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom("1.0.1");
-  api.use(["templating", "less", "meteor", "ddp", "jquery", "fortawesome:fontawesome@4.3.0"]);
+  api.use([
+    "templating",
+    "less",
+    "meteor",
+    "ddp",
+    "fortawesome:fontawesome@4.3.0"
+  ], 'client');
 
-  api.addFiles("client/sidebar.js", ["client"]);
+
   api.addFiles("client/sidebar.html", ["client"]);
+  api.addFiles("client/sidebar.js", ["client"]);
   api.addFiles("client/sidebar.less", ["client"]);
 
   api.export("");
